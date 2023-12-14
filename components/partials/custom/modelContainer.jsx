e561cc306c342cbb3e11fd1de5b8bdb4cbfec1d3
Bestada <alfanmuazharin@gmail.com>
2023-12-14 10:41:05 +0700

    fix:placket

--- components/partials/custom/modelContainer.jsx
+++ components/partials/custom/modelContainer.jsx
@@ -24,7 +24,7 @@ const ModelContainer = ({ category, fabricSelect, styleSelect }) => {
                 setPocket(styleSelect.category)
                 break;
             case "Placket":
-                setPlacket(styleSelect.category)
+                setPlacket(styleSelect.category.toLowerCase())
                 break;
             default:
                 break;
